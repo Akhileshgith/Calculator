55d7d26e89e2285e42ab8801f23346468b99b1cb
Akhilesh Yadav <119729282+Akhileshgith@users.noreply.github.com>
2025-02-14 17:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('calculatorForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operation = document.getElementById('operation').value;
+    let result;
+  
+    switch (operation) {
+      case 'add':
+        result = num1 + num2;
+        break;
+      case 'subtract':
+        result = num1 - num2;
+        break;
+      case 'multiply':
+        result = num1 * num2;
+        break;
+      case 'divide':
+        result = num2 !== 0 ? num1 / num2 : 'Error: Division by zero';
+        break;
+      default:
+        result = 'Invalid operation';
+    }
+  
+    document.getElementById('result').textContent = `Result: ${result}`;
+  });
+  
